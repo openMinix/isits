fa8f8ac5e11044203e499207f6838429ff4dabf8
Vlad Barosan <vlad.barosan@gmail.com>
2013-10-23 10:47:59 +0300

    Initial project

---
+++ sharknet/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharknet::Application.config.secret_key_base = 'ba409e6153b0d058c431f7ef136424b6f2b9cd0be21f56d159ea133f79f1e8fbf3c9e4f8f603cb1c4fa4e9351fc37867ce69eb4b80fb4176e2d3581828b1e759'
---
+++ sharknet/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sharknet::Application.config.session_store :cookie_store, key: '_sharknet_session'
