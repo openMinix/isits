3d290e90ca6ffe295ce6d100afa2d0ca8ac1d7fa
Vlad Barosan <vlad.barosan@gmail.com>
2014-01-11 22:26:06 +0200

    Deleting users and editing them works

--- sharknet/app/controllers/memberships_controller.rb
+++ sharknet/app/controllers/memberships_controller.rb
@@ -1,7 +1,7 @@
 class MembershipsController < Devise::RegistrationsController
   skip_before_filter :require_no_authentication # , :only => :shxow
 
-  load_and_authorize_resource :user, :parent => false, except: [:create]
+  load_and_authorize_resource :user, :parent => false, except: [:create ]
   def create
     build_resource(sign_up_params)
 
@@ -10,12 +10,12 @@ class MembershipsController < Devise::RegistrationsController
       if resource.active_for_authentication?
        # sign_up(resource_name, resource)
 
-        redirect_to :controller => 'timesheets', :action => 'index'
+        redirect_to :controller => 'memberships', :action => 'index'
       else
         #set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
         #expire_data_after_sign_in!
         #respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-        redirect_to :controller => 'timesheets', :action => 'index'
+        redirect_to :controller => 'memberships', :action => 'index'
       end
     else
       clean_up_passwords resource
@@ -23,8 +23,34 @@ class MembershipsController < Devise::RegistrationsController
     end
   end
 
-  def index
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
 
+  def update
+
+    @user = User.find(params[:id])
+    account_update_params.delete(:current_password)
+    if @user.update_with_password(account_update_params)
+        redirect_to :controller => 'memberships', :action => 'index'
+    else
+      clean_up_passwords resource
+      respond_with @user 
+    end
+  end
+
+  # DELETE /resource
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+      redirect_to :controller => 'memberships', :action => 'index'
+    end
+  end
+
+  def index
     @users = User.all;
   end
 end
