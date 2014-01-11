2996701bfb54d47ca490fc1cdda25758e5b7a510
Vlad Barosan <vlad.barosan@gmail.com>
2014-01-11 23:35:00 +0200

    Projects delete working

--- sharknet/app/controllers/projects_controller.rb
+++ sharknet/app/controllers/projects_controller.rb
@@ -58,7 +58,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to projects_url }
+      format.html { redirect_to :back }
       format.json { head :no_content }
     end
   end
